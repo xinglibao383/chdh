fb472a58fdaff3f4e80859b4654c4ab8e81a506a
xinglibao <xinglibao383@gmail.com>
2025-04-25 22:37:51 +0800

    fix bug

--- src/main/java/com/mpw/model/chfx/domain/dto/HidePossibilityPreDTO.java
+++ src/main/java/com/mpw/model/chfx/domain/dto/HidePossibilityPreDTO.java
@@ -2,6 +2,7 @@ package com.mpw.model.chfx.domain.dto;
 
 import com.mpw.model.chfx.domain.entity.GreenArea;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HidePossibilityPreDTO {
@@ -9,11 +10,13 @@ public class HidePossibilityPreDTO {
     private List<GreenArea> blueGreenAreas;
 
     public HidePossibilityPreDTO() {
+        redGreenAreas = new ArrayList<>();
+        blueGreenAreas = new ArrayList<>();
     }
 
     public HidePossibilityPreDTO(List<GreenArea> redGreenAreas, List<GreenArea> blueGreenAreas) {
-        this.redGreenAreas = redGreenAreas;
-        this.blueGreenAreas = blueGreenAreas;
+        this.redGreenAreas = redGreenAreas == null ? new ArrayList<>() : redGreenAreas;
+        this.blueGreenAreas = blueGreenAreas == null ? new ArrayList<>() : blueGreenAreas;
     }
 
     public List<GreenArea> getRedGreenAreas() {
